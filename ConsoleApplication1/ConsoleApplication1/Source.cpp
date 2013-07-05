02929591ffbae04deaa863233bfca6f557db3cd1
JMMcDonagh <11211426@students.lincoln.ac.uk>
2013-07-05 10:44:54 +0100

    first

---
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -0,0 +1,26 @@
+#include <opencv2\highgui\highgui.hpp>
+#include <iostream>
+
+
+int main()
+{
+	cv::Mat image = cv::imread("Data/Face1.JPG", CV_LOAD_IMAGE_COLOR);
+	if(!image.data)
+	{
+		std::cout << "Could not open or find the image"  << std::endl;
+		//system("pause"); //wait for a key press
+		return -1;
+    }
+
+	
+	cv::namedWindow( "Display window", CV_WINDOW_AUTOSIZE); //create a window with the name "MyWindow"
+	cv::moveWindow( "Display window", 300, 100);
+	cv::imshow( "Display window", image); //display the image which is stored in the 'img' in the "MyWindow" window
+
+
+	cv::waitKey(0); //wait infinite time for a keypress
+
+	cv::destroyWindow("MyWindow"); //destroy the window with the name, "MyWindow"
+
+	return 0;
+}
