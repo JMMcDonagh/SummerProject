c8298ebcd2f3dbb927b1fd3bf61b59cf22851601
JMMcDonagh <11211426@students.lincoln.ac.uk>
2013-07-07 23:08:51 +0100

    Revert "face detection"
    This reverts commit fbc9efaef64ffdda8326f1cde60489f677e629c2.

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -1,40 +1,26 @@
-#include <opencv2\imgproc\imgproc.hpp>
-#include <opencv2\objdetect\objdetect.hpp>
 #include <opencv2\highgui\highgui.hpp>
 #include <iostream>
 
+
 int main()
 {
-	cv::CascadeClassifier face_cascade;
-	if (!face_cascade.load("C:/opencv/data/haarcascades/haarcascade_frontalface_alt.xml"))
-	{
-		std::cout << "Couldn't load face_cascade" << std::endl;
-		std::exit(EXIT_FAILURE);
-	}
-
-	cv::Mat image = cv::imread("Data/face4.jpg", CV_LOAD_IMAGE_COLOR);
+	cv::Mat image = cv::imread("Data/Face1.JPG", CV_LOAD_IMAGE_COLOR);
 	if(!image.data)
 	{
-		std::cout << "Could not open or find the image"  << std::endl;		
-		std::exit(EXIT_FAILURE);
+		std::cout << "Could not open or find the image"  << std::endl;
+		//system("pause"); //wait for a key press
+		return -1;
     }
 
-	cv::Mat image_gray;
-	cv::cvtColor(image, image_gray, CV_BGR2GRAY);
-	cv::equalizeHist(image_gray, image_gray);	
 	
-	cv::vector<cv::Rect> faces;	
-	face_cascade.detectMultiScale(image_gray, faces, 1.1, 3, CV_HAAR_SCALE_IMAGE, cv::Size(0, 0), cv::Size(200, 200));
+	cv::namedWindow( "Display window", CV_WINDOW_AUTOSIZE); //create a window with the name "MyWindow"
+	cv::moveWindow( "Display window", 300, 100);
+	cv::imshow( "Display window", image); //display the image which is stored in the 'img' in the "MyWindow" window
 
-	for(unsigned int i = 0; i < faces.size(); i++)
-	{
-		cv::rectangle(image, faces[i], cv::Scalar(255, 255, 0));
-	}
 
-	cv::namedWindow("myWindow", CV_WINDOW_AUTOSIZE);
-	cv::imshow("myWindow", image);
-	
-	cv::waitKey(0);
+	cv::waitKey(0); //wait infinite time for a keypress
+
+	cv::destroyWindow("Display window"); //destroy the window with the name, "MyWindow"
 
 	return 0;
 }
