0fa9e5fbb3953129f6ceed1d5028de6f7362b09e
JMMcDonagh <11211426@students.lincoln.ac.uk>
2013-07-12 14:24:40 +0100

    QWERT

--- ConsoleApplication1/ConsoleApplication1/Source1.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source1.cpp
@@ -30,7 +30,7 @@ int main()
 	float scale = 1.0f;
 	cv::Mat scaleMat = ScaleMatrix(scale);
 
-<<<<<<< HEAD
+
 	float rotZ = 0.0f;
 	cv::Mat rotationMat = RotationMatrix(rotZ);
 
@@ -51,16 +51,6 @@ int main()
 			{				
 				imageResult.at<unsigned char>(y , x) = imageOrginal.at<unsigned char>(result.y , result.x);
 			}
-=======
-	for(int i = 0; i < imageScaled.rows; i++)
-	{
-		for(int j = 0; j < imageScaled.cols; j++)
-		{
-			int ii = cvRound(i / scale);
-			int jj = cvRound(j / scale);
-
-			imageScaled.at<unsigned char>(i , j) = imageOrginal.at<unsigned char>(ii , jj);
->>>>>>> f75570e0805117639e1a673b7e8f03ff17947a3b
 		}
 	}
 
@@ -72,68 +62,6 @@ int main()
 
 	cv::waitKey(0);
 
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-
-
-
-
-	//cv::Mat C = (cv::Mat_<float>(2, 2) << 10.0f, 11.0f, 4.0f, 6.0f);
-	//cv::Mat D = C.inv();
-	////// proof result = identy matrix for both E and F
-	////cv::Mat E = C * D;
-	////cv::Mat F = D * C;
-	//cv::Mat inverseMatrix = Inverse(C);
-
-
-
-
-	//cv::Mat SRMat = ScaleRotationMatrix(1.0f, 0.0f);
-	//cv::Mat inverseSRMat = SRMat.inv();
-
-	//
-
-
-	//std::string imageFile = "Data/face1.jpg";
-
-	//cv::Mat imageOrginal = cv::imread(imageFile, CV_LOAD_IMAGE_GRAYSCALE);
-	//if(!imageOrginal.data)
-	//{
-	//	std::cout << "Could not open or find the image"  << std::endl;		
-	//	std::exit(EXIT_FAILURE);
- //   }
-
-	//
-	//cv::Mat imageScaled = cv::Mat::zeros(imageOrginal.rows, imageOrginal.cols, imageOrginal.type()); 
-
-	//
-
-	//cv::Point center = cv::Point(imageOrginal.cols / 2, imageOrginal.rows / 2);
-	//double angle = 50.0;
-	//double scale = 2.0;
-
-	//
-	//cv::Mat rot_mat = cv::getRotationMatrix2D(center, angle, scale);
-	//
-	//cv::warpAffine(imageOrginal, imageScaled, rot_mat, imageOrginal.size());
-
-	//cv::namedWindow("Source image", CV_WINDOW_AUTOSIZE );
-	//cv::imshow("Source image", imageOrginal );
-
-	//cv::namedWindow("Warp", CV_WINDOW_AUTOSIZE );
-	//cv::imshow("Warp", imageScaled );
-
-
-	//cv::waitKey(0);
-
-
->>>>>>> f75570e0805117639e1a673b7e8f03ff17947a3b
 	return 0;
 }
 
@@ -144,7 +72,6 @@ float Determinant(const cv::Mat& inMat)
 
 cv::Mat Inverse(const cv::Mat& inMat)
 {
-<<<<<<< HEAD
 	float determinant = Determinant(inMat);
 	float det;
 	if(determinant == 0)
@@ -154,14 +81,10 @@ cv::Mat Inverse(const cv::Mat& inMat)
 	else
 	{
 		det = 1.0f / determinant;
-	}
+	}	
 
-=======
-	float det = 1.0f / Determinant(inMat);
-	
->>>>>>> f75570e0805117639e1a673b7e8f03ff17947a3b
 	cv::Mat retMat = (cv::Mat_<float>(2, 2) <<  inMat.at<float>(1 , 1), -inMat.at<float>(0 , 1),
-						   -inMat.at<float>(1 , 0),  inMat.at<float>(0 , 0));	
+											   -inMat.at<float>(1 , 0),  inMat.at<float>(0 , 0));	
 	
 	return det * retMat;
 }
@@ -184,13 +107,8 @@ cv::Mat RotationMatrix(float angle)
 	float cosTheta = cosf(theta);
 	float sinTheta = sinf(theta);
 
-<<<<<<< HEAD
 	return (cv::Mat_<float>(2, 2) <<  cosTheta, -sinTheta,
 		                              sinTheta,  cosTheta);
-=======
-	return (cv::Mat_<float>(2, 2) << scale * cosTheta, scale * -sinTheta,
-		                         scale * sinTheta, scale *  cosTheta);	
->>>>>>> f75570e0805117639e1a673b7e8f03ff17947a3b
 }
 
 cv::Point2f Translation(const cv::Mat& SRMatInv, const cv::Point2f& offset, const cv::Point2f& position)
